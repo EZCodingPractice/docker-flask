fdf0484657e62d621cf2b87fa88a071bc3775261
eduzenet <eduzenet@hotmail.com>
2022-01-11 18:03:43 -0500

    Update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'KEEP LEARNING, KEEP MOVING AHEAD'
+    return 'KEEP LEARNING, KEEP MOVING AHEAD - NEVER STOP'
 
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
